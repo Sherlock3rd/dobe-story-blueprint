d97484c0ad537fb515cd4557a3597e938f0f0123
U-LEGOUTECH\liuweichen <liuweichen@liuweichen-dp.legoutech.cn>
2026-08-12 23:17:28 +0800

    Rename case-board chapters as narrative acts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dobe-story-assets-v14';
+const CACHE_NAME = 'dobe-story-assets-v15';
 const CACHED_ASSETS = [
   './assets/clue-scenes/chase-police.webp',
   './assets/clue-scenes/cargo-ambush-rescue.webp',
