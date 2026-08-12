03616ba5532ef7438eb453411cc5b3578fd3bbe0
U-LEGOUTECH\liuweichen <liuweichen@liuweichen-dp.legoutech.cn>
2026-08-12 22:44:08 +0800

    Split L27 into a new case-board chapter

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dobe-story-assets-v12';
+const CACHE_NAME = 'dobe-story-assets-v13';
 const CACHED_ASSETS = [
   './assets/clue-scenes/chase-police.webp',
   './assets/clue-scenes/cargo-ambush-rescue.webp',
